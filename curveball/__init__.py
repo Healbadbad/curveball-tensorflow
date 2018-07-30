51f0480c0bc9420fc3bf039083a27ed07c15795c
Nathan Blank <Nathan.blank42@gmail.com>
2018-07-30 15:09:15 -0500

    First commit, the optimizer does work - no autotuning

---
+++ curveball/__init__.py
@@ -0,0 +1,46 @@
+# Copyright 2017 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Curveball Optimizer."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+# pylint: disable=unused-import,line-too-long
+from curveball.python import curvature_matrix_vector_products
+#from kfac.python.ops import estimator
+#from kfac.python.ops import fisher_blocks
+#from kfac.python.ops import fisher_factors
+#from kfac.python.ops import layer_collection
+#from kfac.python.ops import loss_functions
+#from kfac.python.ops import op_queue
+from curveball.python import optimizer
+#from kfac.python.ops import utils
+#from kfac.python.ops.kfac_utils import async_inv_cov_update_kfac_opt
+#from kfac.python.ops.kfac_utils import data_reader
+#from kfac.python.ops.kfac_utils import periodic_inv_cov_update_kfac_opt
+#from kfac.python.ops.tensormatch import graph_matcher
+#from kfac.python.ops.tensormatch import graph_search
+
+# pylint: enable=unused-import
+
+# pylint: disable=invalid-name
+#LayerCollection = layer_collection.LayerCollection
+CurveballOptimizer = optimizer.CurveballOptimizer
+#PeriodicInvCovUpdateKfacOpt = periodic_inv_cov_update_kfac_opt.PeriodicInvCovUpdateKfacOpt
+
+#AsyncInvCovUpdateKfacOpt = async_inv_cov_update_kfac_opt.AsyncInvCovUpdateKfacOpt
+
+# pylint: enable=invalid-name, line-too-long
---
+++ curveball/examples/rosenbrock_test.py
@@ -0,0 +1,66 @@
+
+import tensorflow as tf
+import curveball
+#from tf.train import GradientDescentOptimizer
+
+def noisy_rosenbrock(u, v, noise_range=0):
+    noise = tf.random_uniform([1], 0, 1) * noise_range + 1
+    return (1-u) ** 2 + 100 * noise * (v - u ** 2) ** 2
+
+print(noisy_rosenbrock(1,1))
+
+def optimization_loop(optimizer, loss, train_step):
+
+    #loss = noisy_rosenbrock(u, v)
+    init = tf.initialize_all_variables()
+    with tf.train.MonitoredTrainingSession() as sess:
+        sess.run(init)
+        for i in range(5000):
+        #while not sess.should_stop():
+        #    sess.close()
+
+            [loss_, _] = sess.run([loss, train_step])
+            print(loss_)
+          #global_step_, loss_, accuracy_, _ = sess.run(
+          #    [g_step, loss, accuracy, train_op])
+
+            #if i % 1 == 0:
+            #    tf.logging.info("global_step: %d | loss: %f | accuracy: %s",
+            #        global_step_, loss_, accuracy_)
+
+
+
+gd_optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001,
+                name='GradientDescent')
+
+
+u = tf.get_variable("u", dtype=tf.float32, initializer=tf.constant([0.25]))
+v = tf.get_variable("v", dtype=tf.float32, initializer=tf.constant([0.25]))#, trainable=False)
+uv = tf.concat([u, v], axis=0)
+print(uv)
+loss = noisy_rosenbrock(uv[0], uv[1])
+init = tf.initialize_all_variables()
+#with tf.train.MonitoredTrainingSession() as sess:
+#    sess.run(init)
+#    feed = {u:u, v:v}
+#    loss_ = sess.run(loss)
+#    print(loss)
+
+#train_step = gd_optimizer.minimize(loss)
+#optimization_loop(gd_optimizer, loss, train_step)
+
+Hl = tf.hessians(loss, uv)
+print("Hl function:", Hl)
+with tf.Session() as sess:
+    sess.run(init)
+    Hl_ = sess.run(Hl)
+    print(Hl_)
+    
+
+# Curveball Optimizer
+curveball_optimizer = curveball.CurveballOptimizer(learning_rate=1, name='Curveball', input_to_loss=uv)
+train_step = curveball_optimizer.minimize(loss)
+optimization_loop(curveball_optimizer, loss, train_step)
+
+
+
